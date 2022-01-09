d536ec1f116a25623bd55adfa73464fe684e8f96
Popescu Andrei <andreigabrielpopescu556@gmail.com>
2022-01-09 12:21:47 +0200

    Start implementing the results reportinng part and twiking the results

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-from src import NMF
+import matplotlib.pyplot as plt
+import wandb
+import os
+import json
+
+data_folders = {
+    "news": { 
+        "full": "all_news_results/doc_to_topics.json",
+        "topics": "all_news_results/topic_to_words.json"
+    },
+    "trip": {
+        "full": "tripadvisor_hotel_reviews_results/doc_to_topics.json",
+        "topics": "tripadvisor_hotel_reviews_results/topic_to_words.json"
+    }
+}
 
 if __name__ == '__main__':
-    print("Hello World!")
-    print(NMF)
+    # wandb.init(project="neural-matrix-factorization", entity="popescuaaa")
+
+    # Tripadvisor
+    with open(data_folders["news"]["topics"], "r") as f:
+        pass
